1f62357304e6f625d1591810c15ea296ee0800eb
Zeffies <zeffiesmc@gmail.com>
2021-02-11 17:06:13 -0500

    Here's my submission for lab one. Not too bad.

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,5 @@
 # This is a cool IDE
+print("This is a lab that I am doing on my own.")
+print("I really need to learn how to code so I can get a good job.")
+print("Hopefully it doesn't take too long to learn.")
+print("I need an escape character so here's a double quote: \".")
